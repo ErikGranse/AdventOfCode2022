8c075826a8ef9e2af13db93320251c4a3a869bc1
Erik Granse <erik.granse@daugherty.com>
2022-12-02 20:47:55 -0600

    prepped day 3

---
+++ src/net/loonmagnet/Day3.java
@@ -0,0 +1,17 @@
+package net.loonmagnet;
+
+import java.util.List;
+
+public class Day3 {
+
+    public static void main(String... args) {
+
+    }
+
+    private static void loadData() {
+        List<String> strings = Utils.readFile("data/day3.txt");
+
+
+
+    }
+}
