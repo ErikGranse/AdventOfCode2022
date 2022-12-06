a0027fb7be41a246aa63dd3827f1000ec1e7336f
Erik Granse <erik.granse@daugherty.com>
2022-12-05 23:22:12 -0600

    day 6

--- src/net/loonmagnet/java/Day6.java
+++ src/net/loonmagnet/java/Day6.java
@@ -9,16 +9,25 @@ public class Day6 {
     public static void main(String... args) {
 
         var data = Utils.readFile("data/day6.txt");
-        var chars = data.get(0).toCharArray();
 
+        var chars = data.get(0).toCharArray();
+        findMarker(4, chars);
+        findMarker(14, chars);
+    }
+
+    private static void findMarker(int minLength, char[] chars) {
         var pos = 0;
-        Queue<Character> characters = new LinkedList<>(Arrays.asList(' ', ' ', ' ', ' '));
+
+        Queue<Character> characters = new LinkedList<>();
+        for (int i = 0; i < minLength; i ++) {
+            characters.add(' ');
+        }
         for (char val : chars) {
             pos++;
             characters.add(val);
             characters.remove();
             Set<Character> charset = new HashSet<>(characters);
-            if (charset.size() == 4 && pos >= 4) {
+            if (charset.size() == minLength && pos >= minLength) {
                 break;
             }
         }
