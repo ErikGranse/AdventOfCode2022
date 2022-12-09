b24a4eee1c2433710f81476c3db47d3b0c755fa7
Erik Granse <erik.granse@daugherty.com>
2022-12-09 03:07:56 -0600

    removed code

--- src/net/loonmagnet/java/Day9.java
+++ src/net/loonmagnet/java/Day9.java
@@ -2,10 +2,7 @@ package net.loonmagnet.java;
 
 import net.loonmagnet.util.Utils;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static java.lang.Integer.signum;
 import static java.lang.Math.abs;
@@ -18,13 +15,13 @@ public class Day9 {
     }
 
     private static void moveRope(int length) {
-        final List<Knot> knots = getRope(length);
-
+        final List<Knot> knots = new ArrayList<>(Collections.nCopies(length, new Knot(0, 0)));
         final Set<Knot> locations = new HashSet<>();
-        for(String line : Utils.readFile("data/day9.txt")) {
+
+        for (String line : Utils.readFile("data/day9.txt")) {
             final String[] fields = line.split(" ");
             for (int i = 0; i < Integer.parseInt(fields[1]); i++) {
-                knots.set(0, knots.get(0).move(fields[0]));
+                knots.set(0, knots.get(0).move(fields[0])); //move head
                 for (int kn = 1; kn < length; kn++) {
                     knots.set(kn, knots.get(kn).follow(knots.get(kn - 1)));
                 }
@@ -32,12 +29,6 @@ public class Day9 {
             }
         }
         System.out.println(locations.size());
-    }
-
-    static List<Knot> getRope(int length) {
-        final List<Knot> knots = new ArrayList<>();
-        for (int i = 0; i < length; i++) knots.add(new Knot(0,0));
-        return knots;
     }
 }
 
@@ -49,7 +40,8 @@ record Knot(int x, int y) {
             case "D": return new Knot(this.x, this.y - 1);
             case "L": return new Knot(this.x - 1, this.y);
             case "R": return new Knot(this.x + 1, this.y);
-            default: throw new RuntimeException("Didn't expect that direction!");
+            default:
+                throw new RuntimeException("Didn't expect that direction!");
         }
     }
 
